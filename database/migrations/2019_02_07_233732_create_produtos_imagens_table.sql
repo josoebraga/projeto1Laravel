ba07048f17f240dcd5fa0acbf1ff369709e83bf8
josoebraga <42846760+josoebraga@users.noreply.github.com>
2019-05-05 14:38:48 -0300

    database
    database

---
+++ database/migrations/2019_02_07_233732_create_produtos_imagens_table.sql
@@ -0,0 +1,9 @@
+#create table produtos_imagens (id int, caminho varchar(100));
+
+insert projeto1.produtos_imagens (id, caminho) values (1, 12155);
+
+select * from projeto1.produtos_imagens;
+
+delete from projeto1.produtos_imagens where id = 1;
+
+select * from projeto1.produtos_imagens;
